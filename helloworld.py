fe27d759016c2429b81684a15e8040a9244bfcec
Darrell-Devana <darrell.devanaputra@binus.ac.id>
2023-02-14 21:57:46 +0700

    git copilot testing

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,5 @@
-a = "Hello world! My name is"
-b = "Darrell Devana"
-print(a+b)
+#make two string variables that takes input from user and prints it
+name = input("What is your name? ")
+print("Hello, " + name + "!")
+age = input("How old are you? ")
+print("You are " + age + " years old.")
